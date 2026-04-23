b1415ec7ef2d27913693d9cb0ad185684d8f475a
Halimunnisa Shaik <shaikmunni1314@gmail.com>
2026-04-23 19:12:25 +0530

    Update roles in Bio constant

--- Halimunnisa-portfolio/src/data/constants.js
+++ Halimunnisa-portfolio/src/data/constants.js
@@ -5,7 +5,9 @@ import AdminLogin from "../assets/Admin_login.png";
 export const Bio = {
   name: "Halimunnisa Shaik",
   roles: [
-    "Frontend Developer",
+    "MERN Developer"
+    "Devops Engineer"
+   "Frontend Developer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
