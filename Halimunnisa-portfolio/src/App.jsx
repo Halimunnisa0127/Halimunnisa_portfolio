9486820c7a1d37f71dee35feff5e8403c8cafc74
Halimunnisa Shaik <shaikmunni1314@gmail.com>
2026-09-05 16:37:26 +0530

    Update App.jsx

--- Halimunnisa-portfolio/src/App.jsx
+++ Halimunnisa-portfolio/src/App.jsx
@@ -10,7 +10,7 @@ import StartCanvas from "./Components/canvas/Stars";
 import Projects from "./Components/Section/Projects";
 import Contact from "./Components/Section/Contact";
 import Footer from "./Components/Section/Footer";
-import SplashCursor from "./Components/SplashCursor";
+// import SplashCursor from "./Components/SplashCursor";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
